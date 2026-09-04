b6be0fbfaf42cf1aa9d30e623e93b0a28ae12803
Socrates <socratesoduduwa@gmail.com>
2026-09-04 05:21:02 +0100

    Delete FinalProject directory

--- FinalProject/expenses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Group, Member, Expense, Share, Settlement
-
-admin.site.register(Group)
-admin.site.register(Member)
-admin.site.register(Expense)
-admin.site.register(Share)
-admin.site.register(Settlement)
--- FinalProject/expenses/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse
-
-
-class AuthRouteTests(SimpleTestCase):
-    def test_login_route_exists(self):
-        response = self.client.get(reverse('login'))
-        self.assertEqual(response.status_code, 200)
